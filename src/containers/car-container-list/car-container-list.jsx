f06bae3fbffda7990c8417a9b4a4935efbffc65c
Jeffy <abraham_jeffy@yahoo.com>
2021-05-14 03:19:32 +0530

    Update car-container-list.jsx

--- src/containers/car-container-list/car-container-list.jsx
+++ src/containers/car-container-list/car-container-list.jsx
@@ -2,7 +2,7 @@ import CarContainer from "../car-container/car-container";
 
 export default function CarList({ carDetails }) {
   return (
-    <div style={{marginTop:'250px'}}>
+    <div className='car-container-parent' style={{marginTop:'250px'}}>
       {carDetails.map((carData) => {
         return (
           <div>
